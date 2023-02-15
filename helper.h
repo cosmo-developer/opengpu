4a5933d4ad80875b8d9f9494b110d6d6f404ac1a
cosmo-developer <sonuaryan76448002@gmail.com>
2023-02-15 19:49:47 +0530

    glew support

--- helper.h
+++ helper.h
@@ -28,6 +28,7 @@ std::string get_file_contents(const char* filename) {
 static bool EnCounterShaderError(GLuint shader) {
 	GLint isCompiled = 0;
 	glGetShaderiv(shader, GL_COMPILE_STATUS, &isCompiled);
+	
 	if (isCompiled != GL_FALSE) {
 		return false;
 	}
@@ -44,4 +45,27 @@ static bool EnCounterShaderError(GLuint shader) {
 	std::clog << std::endl;
 	glDeleteShader(shader);
 	return true;
+}
+
+
+static void CheckForProgramError(GLuint program) {
+	GLint linked = 0;
+
+	glGetProgramiv(program, GL_LINK_STATUS, &linked);
+
+	if (linked != GL_FALSE) {
+		return;
+	}
+
+	GLint maxLength = 0;
+	glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+
+
+	std::vector<GLchar> errorLog(maxLength);
+
+	glGetProgramInfoLog(program, maxLength, &maxLength, &errorLog[0]);
+
+	glDeleteProgram(program);
+
+	throw std::runtime_error(&errorLog[0]);
 }
--- test.cpp
+++ test.cpp
@@ -7,23 +7,54 @@
 #include<glm/gtc/type_ptr.hpp>
 #include "helper.h"
 
-struct ComputeShader {
-	GLint ID;
-	ComputeShader(std::string source) {
-		
+struct shader_exception {
+	const GLuint ID;
+	const std::string what;
+	shader_exception(GLuint ID, std::string what):ID(ID),what(what) {
+
 	}
 };
 
+struct ComputeShader {
+	GLuint ID;
+	ComputeShader(std::string source) {
+		ID=glCreateShader(GL_COMPUTE_SHADER);
+		
+		const char* shaderSource = source.c_str();
+		glShaderSource(ID, 1, &shaderSource, NULL);
+		glCompileShader(ID);
+		if (EnCounterShaderError(ID)) {
+			ID = 0;
+			return;
+		}
+	}
+
+	bool IsValidShader() {
+		return ID != 0;
+	}
+
+};
+
 struct GPUProgram {
-	const GLint ID;
-	GPUProgram():ID(glCreateProgram()) {}
+	GLuint ID;
+	GPUProgram() {
+		ID = glCreateProgram();
+	}
 
+	void Link() {
+		glLinkProgram(ID);
+		CheckForProgramError(ID);
+	}
 
+	void AttachShader(ComputeShader& shader) {
+		if (!shader.IsValidShader())throw shader_exception(shader.ID,"Invalid shader!");
+
+	}
 };
 
 
 int main(const int argc,const char** argv){
-	if (gladLoadGL()!=0) return -1;
-
+	
+	GLuint program = glCreateProgram();
 	return 0;
 }
